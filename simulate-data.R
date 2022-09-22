5f0d3666388e80603bf53ce6b6fe3920f21e9bc8
ZacharyHimmelberger <zhimmel@gmail.com>
2022-09-22 12:45:32 -0400

    set sample size as a variable

--- simulate-data.R
+++ simulate-data.R
@@ -3,15 +3,18 @@
 # set seed
 set.seed(123)
 
+# set sample size
+n = 100
+
 # simulate variables
-id <- 1:100
+id <- 1:n
 
 gender <- sample(x = c("male", "female", "non-binary"), 
-                 size = 100,
+                 size = n,
                  replace = TRUE, 
                  prob = c(.4, .5, .1))
 
-gpa <- round(rnorm(n = 100, mean = 2.8, sd = .3), 2)
+gpa <- round(rnorm(n = n, mean = 2.8, sd = .3), 2)
 
 # create data.frame
 df <- data.frame(id, gender, gpa)
